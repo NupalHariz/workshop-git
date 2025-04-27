04e484c68c4f608ffcb190ea748e248c96af308d
yogarn <radityanalaa@gmail.com>
2025-04-27 16:39:43 +0700

    feat: add multiplication functionality

--- src/main/java/com/workshop/calculator/Calculator.java
+++ src/main/java/com/workshop/calculator/Calculator.java
@@ -8,10 +8,15 @@ public class Calculator {
     public int subtract(int a, int b) {
         return a - b;
     }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
     
     public static void main(String[] args) {
         Calculator calc = new Calculator();
         System.out.println("2 + 3 = " + calc.add(2, 3));
         System.out.println("5 - 2 = " + calc.subtract(5, 2));
+        System.out.println("2 * 3 = " + calc.multiply(2, 3));
     }
 }
